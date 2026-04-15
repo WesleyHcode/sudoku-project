67cb25275ed154b9ad301318f17c4edd40cf7e18
WesleyHcode <wesleyh.12000@gmail.com>
2026-04-14 22:59:18 -0400

    Update and rename Sudoku.txt to SudokuGenerator.java

--- SudokuGenerator.java
+++ SudokuGenerator.java
@@ -1,38 +1,3 @@
-SudokuGenTest.java:
-------------------
-public class SudokuGenTest {
-    public static void main(String[] args) {
-        int[][] board1 = new int[9][9];
-        SudokuGenerator game1 = new SudokuGenerator();
-        game1.solve(board1, 0, 0);
-        System.out.println(game1.toString(board1));
-        
-        int[][] board2 = new int[9][9];
-        SudokuGenerator game2 = new SudokuGenerator();
-        game2.solve(board2, 0, 0);
-        System.out.println(game2.toString(board2));
-        
-        int[][] board3 = new int[9][9];
-        SudokuGenerator game3 = new SudokuGenerator();
-        game3.solve(board3, 0, 0);
-        System.out.println(game3.toString(board3));
-        
-        int[][] board4 = new int[9][9];
-        SudokuGenerator game4 = new SudokuGenerator();
-        game3.solve(board4, 0, 0);
-        System.out.println("BEFORE TEST");
-        System.out.println(game4.toString(board3));
-        board4[1][2] = 0;
-        board4[3][4] = 0;
-        board4[5][6] = 0;
-        game3.solve(board4, 0, 0);
-        System.out.println("AFTER TEST");
-        System.out.println(game4.toString(board3));
-    }
-}
-
-SudokuGenerator.java:
---------------------
 import java.util.ArrayList;
 import java.util.Arrays;
 
